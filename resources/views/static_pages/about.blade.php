d4b2c97cb15e13206a2080671294197c6294eb4b
alan <855alan@gmail.com>
2022-08-06 21:43:52 +0000

    模板切分

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>about</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title','about')
+@section('content')
+    <h1>about</h1>
+@stop
