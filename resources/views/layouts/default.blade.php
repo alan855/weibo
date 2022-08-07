401ff96591659252d21fa1bda14b918d0f981549
alan <855alan@gmail.com>
2022-08-07 18:30:30 +0000

    切割头尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,21 +8,13 @@
   <title>@yield('title','Weibo App')--- laravel 新手入门教程</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Weibo App</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link" href="/help">Help</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
+
   <div class="container">
-    @yield('content')
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 </body>
 </html>
